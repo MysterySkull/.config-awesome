c0448a12b137af4d646f403d19e66b1128058654
MysterySkull <guillaume.hernandez.1@gmail.com>
2025-01-11 16:09:47 +0100

    cleanup

--- rc.lua
+++ rc.lua
@@ -4,10 +4,12 @@ pcall(require, "luarocks.loader")
 
 -- Standard awesome library
 local gears = require("gears")
-local awful = require("awful")
+
 require("awful.autofocus")
+
 -- Theme handling library
 local beautiful = require("beautiful")
+
 -- Notification library
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
@@ -15,31 +17,11 @@ require("awful.hotkeys_popup.keys")
 
 require("error_handling")
 
-
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
--- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts = {
-    awful.layout.suit.tile,
-    awful.layout.suit.floating,
-    -- awful.layout.suit.tile.left,
-    -- awful.layout.suit.tile.bottom,
-    -- awful.layout.suit.tile.top,
-    -- awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
-    -- awful.layout.suit.spiral,
-    -- awful.layout.suit.spiral.dwindle,
-    -- awful.layout.suit.max,
-    -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier,
-    -- awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
-}
--- }}}
+require("variables.layouts")
 
 require("bindings.global")
 
