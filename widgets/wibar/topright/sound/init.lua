268c21f5cffde4cbf2056e9d3bc2810de360d73e
MysterySkull <guillaume.hernandez.1@gmail.com>
2025-02-07 00:48:56 +0100

    sound widget function added

--- widgets/wibar/topright/sound/init.lua
+++ widgets/wibar/topright/sound/init.lua
@@ -1,22 +1,67 @@
 local awful = require('awful')
+local gears = require('gears')
 local wibox = require('wibox')
+local naughty = require('naughty')
 
 local clk_widget = { mt = {} }
 
-local function new(args)
+local function new()
+
+   local get_volume = function(tooltip)
+
+      awful.spawn.easy_async_with_shell('pulsemixer --get-mute', function(stdout)
+         if(stdout == "0\n") then
+            awful.spawn.easy_async_with_shell('pulsemixer --get-volume', function(stdout)
+               tooltip.text = stdout
+            end)
+         else
+            tooltip.text = "Mute"
+         end
+      end)
+   end
+
+   local w_btn = gears.table.join(
+      awful.button({ }, 1,
+         function()
+            awful.spawn.easy_async_with_shell('pulsemixer --toggle-mute', function() end)
+            awesome.emit_signal("volume::changed")
+         end
+      ),
+      awful.button({ }, 4,
+         function()
+            awful.spawn.easy_async_with_shell('pulsemixer --change-volume +1', function() end)
+            awesome.emit_signal("volume::changed")
+         end
+      ),
+      awful.button({ }, 5,
+         function()
+            awful.spawn.easy_async_with_shell('pulsemixer --change-volume -1', function() end)
+            awesome.emit_signal("volume::changed")
+         end
+      )
+   )
 
    local sound_icon = "\u{f057e} "
-   local volume = awful.widget.watch('bash -c "pulsemixer --get-volume"', 0.5)
 
-
-   return wibox.widget{
-      {
-         text = sound_icon,
-         widget = wibox.widget.textbox
-      },
-      volume,
-      widget = wibox.layout.fixed.horizontal 
+   local w = wibox.widget{
+      text = sound_icon,
+      buttons = w_btn,
+      widget = wibox.widget.textbox
    }
+
+   local w_tooltip = awful.tooltip{
+      objects = { w },
+   }
+
+   w:connect_signal('mouse::enter', function()
+      get_volume(w_tooltip)
+   end)
+
+   awesome.connect_signal('volume::changed', function()
+      get_volume(w_tooltip)
+   end)
+
+   return w
 end
 
 function clk_widget.mt:__call(...)
